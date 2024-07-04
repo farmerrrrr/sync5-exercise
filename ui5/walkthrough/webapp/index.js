967aa5b048c3ccefe60e6fab1bbd236071528fb2
farmerrrrr <hansungff3846@gmail.com>
2024-07-04 16:52:36 +0900

    [UI5] Step 4: XML Views

--- ui5/walkthrough/webapp/index.js
+++ ui5/walkthrough/webapp/index.js
@@ -1,9 +1,9 @@
 sap.ui.define([
-    "sap/m/Text"
-], (Text) => {
+    "sap/ui/core/mvc/XMLView"
+], (XMLView) => {
 	"use strict";
 
-    new Text({
-        text: "Hello World"
-    }).placeAt("content");      // index.html line 22 body id
+    XMLView.create({
+        viewName: "ui5.walkthrough.view.App"
+    }).then((oView) => oView.placeAt("content"));
 });
