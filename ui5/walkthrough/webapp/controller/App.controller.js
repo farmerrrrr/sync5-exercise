c0dec83cf00248d993fbcd1200110d277bb1d634
farmerrrrr <hansungff3846@gmail.com>
2024-07-05 12:07:42 +0900

    [UI5] Step 7: JSON Model

--- ui5/walkthrough/webapp/controller/App.controller.js
+++ ui5/walkthrough/webapp/controller/App.controller.js
@@ -1,11 +1,22 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], (Controller, MessageToast) => {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], (Controller, MessageToast, JSONModel) => {
     "use strict";
 
     // Controller.extend() 으로 controller.App에 대한 내용 확장
     return Controller.extend("ui5.walkthrough.controller.App", {
+        onInit(){
+            const oData = {
+                recipient: {
+                    name: "World"
+                }
+            };
+            const oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+
         onShowHello() {
             MessageToast.show("Hello World");
         }
