e513b68eed19f227e5e9dcc2878d39b53c7825c0
farmerrrrr <hansungff3846@gmail.com>
2024-07-05 10:43:59 +0900

    [UI5] Step 6: Modules

--- ui5/walkthrough/webapp/controller/App.controller.js
+++ ui5/walkthrough/webapp/controller/App.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict";
 
     // Controller.extend() 으로 controller.App에 대한 내용 확장
     return Controller.extend("ui5.walkthrough.controller.App", {
         onShowHello() {
-            alert("Hello World");
+            MessageToast.show("Hello World");
         }
     });
 });
