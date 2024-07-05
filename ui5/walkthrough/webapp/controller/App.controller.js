b827d142946598e94c8f97b2ef5a2eedbc2915e2
farmerrrrr <hansungff3846@gmail.com>
2024-07-05 16:52:30 +0900

    [UI5] Step 9: Component Configuration

--- ui5/walkthrough/webapp/controller/App.controller.js
+++ ui5/walkthrough/webapp/controller/App.controller.js
@@ -1,29 +1,11 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], (Controller, MessageToast, JSONModel, ResourceModel) => {
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict";
 
     // Controller.extend() 으로 controller.App에 대한 내용 확장
     return Controller.extend("ui5.walkthrough.controller.App", {
-        onInit(){
-            const oData = {
-                recipient: {
-                    name: "World"
-                }
-            };
-            const oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-
-            // view에 리소스 모델(언어) 연결
-            const i18nModel = new ResourceModel({
-                bundleName: "ui5.walkthrough.i18n.i18n"
-            })
-            this.getView().setModel(i18nModel, "i18n");
-        },
-
         onShowHello() {
             const oBundle = this.getView().getModel("i18n").getResourceBundle();
             const sRecipient = this.getView().getModel().getProperty("/recipient/name");
