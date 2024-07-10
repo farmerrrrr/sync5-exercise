ca20f87530fd09d3675ef0a021f66dca845b8bcc
farmerrrrr <hansungff3846@gmail.com>
2024-07-10 09:44:33 +0900

    [UI5] Step 15: Nested Views #walkthrough

--- ui5/walkthrough/webapp/controller/App.controller.js
+++ ui5/walkthrough/webapp/controller/App.controller.js
@@ -6,12 +6,6 @@ sap.ui.define([
 
     // Controller.extend() 으로 controller.App에 대한 내용 확장
     return Controller.extend("ui5.walkthrough.controller.App", {
-        onShowHello() {
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-            MessageToast.show(sMsg);
-        }
+    
     });
 });
---
+++ ui5/walkthrough/webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
+    "use strict";
+
+    // Controller.extend() 으로 controller.App에 대한 내용 확장
+    return Controller.extend("ui5.walkthrough.controller.HelloPanel", {
+        onShowHello() {
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+            MessageToast.show(sMsg);
+        }
+    });
+});
