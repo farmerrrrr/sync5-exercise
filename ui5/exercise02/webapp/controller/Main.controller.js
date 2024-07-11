69dd9f3873e4fac86921742218a3dfbfa35ec633
farmerrrrr <hansungff3846@gmail.com>
2024-07-11 12:48:28 +0900

    [UI5] 실습02 - Main.controller 로직 일부 수정 #fe02

--- ui5/exercise02/webapp/controller/Main.controller.js
+++ ui5/exercise02/webapp/controller/Main.controller.js
@@ -6,9 +6,9 @@ function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("sync.d14.exercise02.controller.Main", {
-        // 나눗셈 체크 함수
-        isDivision(op) {
-            return op === "/";
+        // 나눗셈 유효성 검증 함수
+        isInvalid(op, n2) {
+            return (op === "/" && n2 === 0);
         },
 
         // 계산 함수
@@ -24,24 +24,26 @@ function (Controller, JSONModel) {
         // 결과값 도출 (데이터 가져오고 -> 0 나눗셈 체크하고 -> 계산)
         getResult(obj) {
             // input 데이터 load
-            var iNum1 = Number(obj.number1), iNum2 = Number(obj.number2);
-            var sOper = obj.operator;
+            var iNum1 = Number(obj.getProperty('/number1')),
+                iNum2 = Number(obj.getProperty('/number2'));
+            var sOper = obj.getProperty('/operator');
 
-            // 계산 전 0 나눗셈 체크
-            if (this.isDivision(sOper)) 
-                if (iNum2 === 0) {
-                    alert("나누는 수가 0이면 안 됩니다.");
-                    return;
-                }
+            // 계산 전 0 나눗셈 유효성 체크
+            // 0으로 나누려고 시도 시 결과 값은 0으로 할당
+            if (this.isInvalid(sOper, iNum2)) {
+                alert("나누는 수가 0이면 안 됩니다.");
+                obj.setProperty('/result', 0);
+                return;
+            }
 
             // 계산 로직 수행
-            obj.result = this.calculator(sOper, iNum1, iNum2);
+            const result = this.calculator(sOper, iNum1, iNum2);
+            obj.setProperty('/result', result);
         },
 
         // 내부 연산자 설정 함수
         setOperator(op) {
-            var oModel = this.getView().getModel();
-            oModel.oData.operator = op;
+            this.getView().getModel().setProperty('/operator', op);
         },
 
         // [이벤트 함수] 버튼 설정 이벤트 함수 연산자별 분리
@@ -61,7 +63,7 @@ function (Controller, JSONModel) {
         // [이벤트 함수] 계산 결과 Fragment view 띄우기
         async onOpenResultDialog() {
             // JSON 객체 가져와서 결과값 도출 함수 getresult() 호출
-            this.getResult(this.getView().getModel().oData);
+            this.getResult(this.getView().getModel());
             
             // 계산 후 Fragment load -> open
             this.oDialog ??= await this.loadFragment({
