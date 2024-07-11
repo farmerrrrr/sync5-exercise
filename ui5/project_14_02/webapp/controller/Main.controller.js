2f505de608d10b18c238b669946d5cfd05230f39
farmerrrrr <hansungff3846@gmail.com>
2024-07-11 16:45:24 +0900

    [UI5] select 태그에 모델 바인딩 #prj02

--- ui5/project_14_02/webapp/controller/Main.controller.js
+++ ui5/project_14_02/webapp/controller/Main.controller.js
@@ -1,12 +1,23 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
-function (Controller) {
+function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("sync.d14.project1402.controller.Main", {
-        onInit: function () {
+        onInit() {
+            // list에 배열 데이터로 모델 생성 -> 뷰에 미리 바인딩
+            var oJsonData = { 
+                list: [
+                    { gender: "M", text: "남자" },
+                    { gender: "F", text: "여자" }
+                ]
+            };
 
+            // 모델 생성 후 뷰에 바인딩
+            this.getView().setModel(new JSONModel(oJsonData));
+            this.byId("idSelect").bindElement("/");
         },
 
         onClick() {
